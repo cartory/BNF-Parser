7c3a39d9babb596c33f5ba177fe17a10400fed88
cartory <xXcartoryXx@gmail.com>
2022-01-25 05:39:46 -0400

    update calculadora > warnings

--- src/App.java
+++ src/App.java
@@ -1,21 +1,21 @@
 import Expr.*;
 
 public class App {
-    private static void pruebaAnalex() {
-        String ProgFuente = "99 3. .25 32.78 , ( ) + - * / pi e sen cos sqrt logb";
-        Cinta C = new Cinta(ProgFuente);
+    // private static void pruebaAnalex() {
+    // String ProgFuente = "99 3. .25 32.78 , ( ) + - * / pi e sen cos sqrt logb";
+    // Cinta C = new Cinta(ProgFuente);
 
-        PError E = new PError();
-        Analex A = new Analex(C, E);
-        do {
-            System.out.println(A.Preanalisis() + " Lexem=" + (char) 34 + A.lexema() +
-                    (char) 34);
-            A.avanzar();
-        } while (A.preNom() != Token.FIN);
+    // PError E = new PError();
+    // Analex A = new Analex(C, E);
+    // do {
+    // System.out.println(A.Preanalisis() + " Lexem=" + (char) 34 + A.lexema() +
+    // (char) 34);
+    // A.avanzar();
+    // } while (A.preNom() != Token.FIN);
 
-        if (E.hayError())
-            System.out.println(E.getErrorMsj());
-    }
+    // if (E.hayError())
+    // System.out.println(E.getErrorMsj());
+    // }
 
     public static void main(String[] args) throws Exception {
         // pruebaAnalex();
